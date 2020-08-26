02aa49909f39a55685ce0d40bdac27604cb9a2d9
Jason Davis <jason@jasonericdavis.dev>
2020-08-25 21:27:47 -0400

    Fixed token refreshing Fixed token refreshing and now /api/user works correctly

--- pages/api/refresh.ts
+++ pages/api/refresh.ts
@@ -1,36 +1,40 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { getTokenFromCookies, createAuthToken, verify } from "../../lib/server/token";
-
+import {
+  getTokenFromCookies,
+  createAuthToken,
+  verify
+} from "../../lib/server/token";
+import { User } from "../../interfaces";
 
 // @TODO create a middleware for getting the user from the token
-export default (req:NextApiRequest, res:NextApiResponse) => {
-    if(req.method != 'POST') {
-        res.status(404).end()
-        return
+export default (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method !== "POST") {
+    res.status(404).end();
+    return;
+  }
+
+  try {
+    const accessToken = getTokenFromCookies(req);
+    if (!accessToken) {
+      res.status(401).end();
+      return;
     }
 
-    try {
-        const accessToken = getTokenFromCookies(req)
-        if(!accessToken) {
-            res.status(401).end()
-            return
-        }
+    // @TODO: check to make sure the token hasn't expired
+    let decoded: { user: User } = verify(accessToken, (err) => {
+      console.log(err);
+      res.status(401).end();
+      return;
+    }) as { user: User };
 
-        // @TODO: check to make sure the token hasn't expired
-        let decoded = verify(accessToken, (err) => {
-            console.log(err)
-            res.status(401).end()
-            return
-        })
-
-        if(decoded) {
-            createAuthToken(res, user)
-            res.status(200).end()
-            return
-        } 
-        res.status(401).end()
-    } catch(e) {
-        console.log(e)
-        res.status(401).end()
+    if (decoded) {
+      createAuthToken(res, decoded.user);
+      res.status(200).end();
+      return;
     }
-}
+    res.status(401).end();
+  } catch (e) {
+    console.log(e);
+    res.status(401).end();
+  }
+};
